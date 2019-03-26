8d8d2ccca1b88ee98e4360db7ec36deb9776045c
gulfa <gunnarroe@gmail.com>
2019-03-26 11:25:02 +0200

    Adding the auth file

---
+++ ckanext/validator/auth.py
@@ -0,0 +1,9 @@
+import ckan.plugins.toolkit as toolkit
+from ckanext.restricted import logic
+
+from logging import getLogger
+log = getLogger(__name__)
+
+def validator_manual_validation(context, resource):
+
+    return {"success": True}
